8ef8644877fcabba94e606fc9ccc2e60afa9148f
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-14 10:19:29 +0200

    Add project files.

---
+++ WebaruhazTermek/termek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebaruhazTermek
+{
+    public class Termek
+    {
+        public string Nev { get; set; }
+
+        private int egysegar;
+        public int Egysegar
+        {
+            get { return egysegar; }
+            set
+            {
+                if (value < 1)
+                    egysegar = 1;
+                else
+                    egysegar = value;
+            }
+        }
+
+        private int keszlet;
+        public int Keszlet
+        {
+            get { return keszlet; }
+            set
+            {
+                if (value < 0)
+                    keszlet = 0;
+                else
+                    keszlet = value;
+            }
+        }
+    }
+}
