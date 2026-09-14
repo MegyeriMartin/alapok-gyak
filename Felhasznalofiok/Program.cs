9af91f71509bf2e263d3f015f6798fc339ce97b8
MegyerIMartin <megyeri.martin.2022d@telekimezotur.hu>
2026-09-14 11:18:11 +0200

    harmadik

---
+++ Felhasznalofiok/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FelhasznaloFiok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Felhasznalo felhasznalo = new Felhasznalo();
+
+            felhasznalo.Felhasznalonev = "Péter";
+
+            Console.WriteLine("Felhasználónév: " + felhasznalo.Felhasznalonev);
+
+            felhasznalo.JelszoModositas("123456", "abcdef");
+
+            felhasznalo.JelszoModositas("rossz", "ghijkl");
+
+            felhasznalo.JelszoModositas("abcdef", "123");
+        }
+    }
+}
---
+++ Felhasznalofiok/felhasznalo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FelhasznaloFiok
+{
+    public class Felhasznalo
+    {
+        private string jelszo = "123456";
+
+        public string Felhasznalonev { get; set; }
+
+        public void JelszoModositas(string regiJelszo, string ujJelszo)
+        {
+            if (regiJelszo == jelszo && ujJelszo.Length >= 6)
+            {
+                jelszo = ujJelszo;
+                Console.WriteLine("A jelszó sikeresen módosítva!");
+            }
+            else
+            {
+                Console.WriteLine("A jelszó módosítása sikertelen!");
+            }
+        }
+    }
+}
